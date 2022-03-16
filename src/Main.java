e41fa453cc4946d2009ace0cd2a68febc1bef22e
Karol Profic <karolprofic@protonmail.com>
2022-03-16 09:58:01 +0100

    Dodanie wyswietlania ID

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
 	public static void main(String[] args) {
+		System.out.println("Nazwa grupy: SrodowaNoc, GitID: karolprofic");
 		System.out.println("Nazwa grupy: SrodowaNoc, GitID Leadera: Mafia193");
 	}
 }
