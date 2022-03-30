18e7977754fa2f87e5d79cd23c52362895cbf974
MateuszMatusiak <mateusz.matusiak000@gmail.com>
2022-03-30 09:59:14 +0200

    Implementacja działań

--- src/Arithmetics.java
+++ src/Arithmetics.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class Arithmetics {
+public class Arithmetics implements IArithmeticsAdd, IArithmeticsDiff, IArithmeticsMult, IArithmeticsDiv {
+    @Override
+    public double Addition(double A, double B) {
+        return A + B;
+    }
+
+    @Override
+    public double Difference(double A, double B) {
+        return A - B;
+    }
+
+    @Override
+    public double Division(double A, double B) {
+        return A / B;
+    }
+
+    @Override
+    public double Multiplication(double a, double b) {
+        return a * b;
+    }
 }
