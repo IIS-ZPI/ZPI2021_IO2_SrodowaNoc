3fbb5438240035dbd1c650417bdc11e1904f4dee
root <root@DESKTOP-DG5VMGR.localdomain>
2022-06-08 09:45:17 +0200

    Maven

---
+++ src/module-info.java
@@ -0,0 +1,12 @@
+module NBP {
+
+	requires javafx.graphics;
+	requires javafx.controls;
+	requires javafx.media;
+	requires javafx.base;
+	requires javafx.web;
+	requires javafx.swing;
+	requires javafx.fxml;
+
+	opens ZPI;
+}
